e5768fe191bc6664f19bd97318bd28d1201664ad
goku1989 <goku_1989@163.com>
2021-01-31 15:27:22 +0800

    categoryInfo

--- goku-dbmall-goods-service/src/main/java/com/goku/dbmall/goods/dao/mapper/categoryInfoMapper.java
+++ goku-dbmall-goods-service/src/main/java/com/goku/dbmall/goods/dao/mapper/categoryInfoMapper.java
@@ -3,15 +3,4 @@ package com.goku.dbmall.goods.dao.mapper;
 import com.goku.dbmall.goods.dao.po.categoryInfo;
 
 public interface categoryInfoMapper {
-    int deleteByPrimaryKey(Long id);
-
-    int insert(categoryInfo record);
-
-    int insertSelective(categoryInfo record);
-
-    categoryInfo selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(categoryInfo record);
-
-    int updateByPrimaryKey(categoryInfo record);
 }
