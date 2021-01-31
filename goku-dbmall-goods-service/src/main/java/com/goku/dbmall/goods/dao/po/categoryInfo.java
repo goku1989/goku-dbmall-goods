a72a5d8e44c40fb35247ba329abaead9bc716bce
goku1989 <goku_1989@163.com>
2021-01-31 15:26:25 +0800

    categoryInfo

---
+++ goku-dbmall-goods-service/src/main/java/com/goku/dbmall/goods/dao/po/categoryInfo.java
@@ -0,0 +1,28 @@
+package com.goku.dbmall.goods.dao.po;
+
+import com.goku.foundation.util.BasePO;
+import lombok.Data;
+
+@Data
+public class categoryInfo extends BasePO {
+
+    private String gkcode;
+
+    private String name;
+
+    private String code;
+
+    private String type;
+
+    private String parentGkcode;
+
+    private String logo;
+
+    private String slogan;
+
+    private String catImage;
+
+    private String bgColor;
+
+
+}
