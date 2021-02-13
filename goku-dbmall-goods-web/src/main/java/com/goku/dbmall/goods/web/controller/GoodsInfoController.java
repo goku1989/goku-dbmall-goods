f2558d0f34fd8ce6e0ecc30c47c6151d8a8a70c7
goku1989 <goku_1989@163.com>
2021-02-14 00:42:40 +0800

    商品

--- goku-dbmall-goods-web/src/main/java/com/goku/dbmall/goods/web/controller/GoodsInfoController.java
+++ goku-dbmall-goods-web/src/main/java/com/goku/dbmall/goods/web/controller/GoodsInfoController.java
@@ -1,17 +1,14 @@
 package com.goku.dbmall.goods.web.controller;
 
-import com.goku.dbmall.goods.dto.CategoryInfoDTO;
 import com.goku.dbmall.goods.dto.GoodsInfoDTO;
 import com.goku.dbmall.goods.service.GoodsInfoService;
 import com.goku.foundation.response.BaseResponse;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/goodsInfo")
 public class GoodsInfoController {
@@ -22,5 +19,10 @@ public class GoodsInfoController {
     @PostMapping(value = "insertGoods", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public BaseResponse<Boolean> insertGoods(@RequestBody GoodsInfoDTO goodsInfoDTO) {
         return new BaseResponse<>(goodsInfoService.insertGoods(goodsInfoDTO));
+    }
+
+    @GetMapping(value = "getGoods", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public BaseResponse<Integer> getGoods() {
+        return new BaseResponse<>(1);
     }
 }
